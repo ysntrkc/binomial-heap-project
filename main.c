ce360a89d826ad739b46a76ce668b316bdaad844
ysntrkc <yasin.trkc.142@gmail.com>
2021-01-14 15:25:39 +0300

    a little more progress. and I changed files.

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <string.h>
 
 int main() {
 
-    char fileList[50][100];
+    char fileList[50][100], c;
 
     FILE *filePtr;
     struct dirent *de;
@@ -21,8 +22,26 @@ int main() {
 
 //    printf("%s", fileList[0]);
 
-    for (int i = 0; strcmp(fileList[i], NULL) == 0; i++) {
-        printf("%s\n", fileList[i]);
+    for (int i = 2; strcmp(fileList[i], "\0") != 0; i++) {
+        char fileName[] = "files/";
+
+        strcat(fileName, fileList[i]);
+        filePtr = fopen(fileName, "r");
+        if (filePtr == NULL) {
+            printf("Cannot open file \n");
+            exit(0);
+        }
+
+        printf("\n%s\n", fileList[i]);
+
+        // Read contents from file
+        c = fgetc(filePtr);
+        while (c != EOF) {
+            printf("%c", c);
+            c = fgetc(filePtr);
+        }
+
+        fclose(filePtr);
     }
 
 
