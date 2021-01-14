1691f0f10d74d7e62c2a70c5f60d711bef65ccde
ysntrkc <yasin.trkc.142@gmail.com>
2021-01-14 14:38:25 +0300

    progress

--- main.c
+++ main.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
 
 int main() {
-    printf("Hello, World!\n");
+
+    char fileList[50][100];
+
+    FILE *filePtr;
+    struct dirent *de;
+    DIR *dr = opendir("files");
+
+    if (dr == NULL) {
+        printf("Could not open current directory");
+        return 0;
+    }
+
+    for (int i = 0; (de = readdir(dr)) != NULL; i++) {
+        strcpy(fileList[i], de->d_name);
+    }
+
+//    printf("%s", fileList[0]);
+
+    for (int i = 0; strcmp(fileList[i], NULL) == 0; i++) {
+        printf("%s\n", fileList[i]);
+    }
+
+
+    closedir(dr);
     return 0;
 }
