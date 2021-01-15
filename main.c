4a2571c17e63de65d5f757a35ec365d033b36d9e
ysntrkc <yasin.trkc.142@gmail.com>
2021-01-15 17:55:32 +0300

    this time there is more progress than the last commit.

--- main.c
+++ main.c
@@ -3,48 +3,77 @@
 #include <dirent.h>
 #include <string.h>
 
+struct fileInfo {
+    char fileName[50];
+    int recurrence;
+};
+
 int main() {
 
-    char fileList[50][100], c;
+    int counter;
+    struct fileInfo fileList[50];
+    char keyword[30] = "\0";
+    char currentWord[30] = "\0";
+    char currentChar;
 
     FILE *filePtr;
-    struct dirent *de;
-    DIR *dr = opendir("files");
+    struct dirent *dirStr;
+    DIR *dirPtr = opendir("files");
 
-    if (dr == NULL) {
-        printf("Could not open current directory");
+    printf("Enter a keyword:");
+    scanf("%s", keyword);
+
+    for (int i = 0; i < 50; i++)
+        strcpy(fileList[i].fileName, "\0");
+
+    if (dirPtr == NULL) {
+        printf("Could not open current directory!");
         return 0;
     }
 
-    for (int i = 0; (de = readdir(dr)) != NULL; i++) {
-        strcpy(fileList[i], de->d_name);
-    }
+    for (int i = 0; (dirStr = readdir(dirPtr)) != NULL; i++)
+        strcpy(fileList[i].fileName, dirStr->d_name);
+    closedir(dirPtr);
 
-//    printf("%s", fileList[0]);
+    for (int i = 0; strcmp(fileList[i].fileName, "\0") != 0; i++) {
+        char fileName[56] = "files/";
+        counter = 0;
 
-    for (int i = 2; strcmp(fileList[i], "\0") != 0; i++) {
-        char fileName[] = "files/";
+        if (strcmp(fileList[i].fileName, ".") == 0 || strcmp(fileList[i].fileName, "..") == 0)
+            continue;
 
-        strcat(fileName, fileList[i]);
+        strcat(fileName, fileList[i].fileName);
         filePtr = fopen(fileName, "r");
         if (filePtr == NULL) {
-            printf("Cannot open file \n");
+            printf("Cannot open file!");
             exit(0);
         }
 
-        printf("\n%s\n", fileList[i]);
+//        printf("\n%s\n", fileList[i].fileName);
 
-        // Read contents from file
-        c = fgetc(filePtr);
-        while (c != EOF) {
-            printf("%c", c);
-            c = fgetc(filePtr);
-        }
+        do {
+            currentChar = fgetc(filePtr);
 
+            if ((currentChar >= 65 && currentChar <= 90) || (currentChar >= 97 && currentChar <= 122)) {
+                strcat(currentWord, (char[2]) { (char) currentChar, '\0' });
+//                printf("%c", currentChar);
+            }
+            else {
+                if (stricmp(currentWord, keyword) == 0) {
+                    counter++;
+                }
+
+                strcpy(currentWord, "\0");
+                continue;
+            }
+
+        } while (currentChar != EOF);
+
+        fileList[i].recurrence = counter;
+//        printf("%d", fileList[i].recurrence);
         fclose(filePtr);
     }
 
 
-    closedir(dr);
     return 0;
 }
